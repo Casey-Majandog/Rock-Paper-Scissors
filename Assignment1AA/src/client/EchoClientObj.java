dcc571dc7f958914bfe86496e65f8fabf22d310e
karmanbadhesha_sait <karmanbadhesha@edu.sait.ca>
2020-01-21 17:30:44 -0700

    Back end for GUI

--- Assignment1AA/src/client/EchoClientObj.java
+++ Assignment1AA/src/client/EchoClientObj.java
@@ -5,90 +5,114 @@
  */
 package client;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Date;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
 
-import javax.swing.JOptionPane;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import utility.InputListener;
 import utility.Message;
+
+import java.util.*;
 
 /**
  * @author dwatson
  * @version 1.0
  * 
- * To determine what your IP address in windows - open a command line dialog
- * box and type at the prompt "ipconfig".
+ *          To determine what your IP address in windows - open a command line
+ *          dialog box and type at the prompt "ipconfig".
  * 
- * in Linux type "ifconfig etho"
+ *          in Linux type "ifconfig etho"
  * 
  */
 
-public class EchoClientObj
-{
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
+public class EchoClientObj extends Application {
+
+	Stage window;
+	Scene loginScene, menuScreen;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+			// BorderPane root = new BorderPane();
+			loginScene = new Scene(root, 400, 400);
+			loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(loginScene);
+			window.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void disconnectServer() {
+		
+	}
+	public static boolean connectServer(String user, String ip) {
 		String text = "";
-		String user = "";
 		String logOff = "";
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
-		
-		try
-		{
-			String ip = JOptionPane.showInputDialog("Enter ip address");
-			//int pn = Integer.parseInt(JOptionPane.showInputDialog("Enter port number"));
+		InputListener lis;
+
+		try {
+			// int pn = Integer.parseInt(JOptionPane.showInputDialog("Enter port number"));
+
+			Socket socket = new Socket(ip, 3333);
+
+			// Create an object output stream to send the message to server.
+			OutputStream os = socket.getOutputStream();
+			oos = new ObjectOutputStream(os);
 			
-			Socket socket = new Socket(ip,3333);
 			
-			//Create an object output stream to send the message to server.
-            OutputStream os = socket.getOutputStream();
-            oos = new ObjectOutputStream(os);
-            
-            //Create an object input stream to catch the echo message from the server.
-            InputStream is = socket.getInputStream();
-            ois = new ObjectInputStream(is);
-			
-			user = JOptionPane.showInputDialog("Enter User Name");
-			
-			while(!logOff.equals("quit"))
-			{
-				text = JOptionPane.showInputDialog("Enter Message");
-				
-				Message msg = new Message(user,text,new Date());				
-				
-				oos.writeObject(msg);
-				
-				msg = (Message)ois.readObject();
-				JOptionPane.showMessageDialog(null,msg.toString());
-				
-				logOff = JOptionPane.showInputDialog("Enter \"quit\" to end connection");
-			}
-			
-			//close all sockets and streams
-            socket.close();
-            ois.close();
-            oos.close();
-		}
-		catch (UnknownHostException e)
-		{
+			//gui is the listener
+//
+//			// Create an object input stream to catch the echo message
+//			// from the server
+//			InputStream is = socket.getInputStream();
+//			ois = new ObjectInputStream(is);
+
+//			while (!logOff.equals("quit")) {
+//
+//				text = JOptionPane.showInputDialog("Enter Message");
+//
+//				Message msg = new Message(user, text, new Date());
+//
+//				oos.writeObject(msg);
+//
+//				msg = (Message) ois.readObject();
+//				JOptionPane.showMessageDialog(null, msg.toString());
+//
+//				logOff = JOptionPane.showInputDialog("Enter \"quit\" to end connection");
+//			}
+//
+//			// close all sockets and streams
+//			socket.close();
+//			ois.close();
+//			oos.close();
+			System.out.println("CONNECTED");
+			if (socket.isConnected()) {
+				return true;
+			} 
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
+		return false;
+
+//		 catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
---
+++ Assignment1AA/src/utility/Container.java
@@ -0,0 +1,63 @@
+package utility;
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import client.EchoClientObj;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Container {
+
+	@FXML
+	private Button clearButton, connectButton;
+	@FXML
+	private TextField displayName, serverIP;
+	@FXML
+	private MenuItem quitMenu;
+
+	@FXML
+	public void handleClearButtonAction(ActionEvent event) {
+		displayName.setText("");
+		serverIP.setText("");
+	}
+
+	@FXML
+	public void handleConnectButtonAction(ActionEvent event) throws IOException {
+		if (EchoClientObj.connectServer(displayName.getText(), serverIP.getText())) {
+
+			Parent gameViewParent = FXMLLoader.load(getClass().getClassLoader().getResource("client/GameScreen.fxml"));
+			Scene gameScene = new Scene(gameViewParent);
+
+			// get existing window from loginScene
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(gameScene);
+			window.show();
+		} else {
+			System.out.println("error");
+		}
+	}
+
+	@FXML
+	public void handleQuitMenuAction(ActionEvent event) {
+		Platform.exit();
+		// http://www.learningaboutelectronics.com/Articles/How-to-add-functionality-to-a-menu-in-JavaFX.php
+	}
+	
+	@FXML
+	public void handleDisconnectButtonAction(ActionEvent event) {
+		Platform.exit();
+		// http://www.learningaboutelectronics.com/Articles/How-to-add-functionality-to-a-menu-in-JavaFX.php
+	}
+
+}
