0e123c7fe8c138dfe198e936d2fd0d451b1acba1
karmanbadhesha <48081699+karmanbadhesha@users.noreply.github.com>
2020-01-26 17:09:12 -0700

    Combined controller + gui to one class

--- Assignment1AA/src/utility/Container.java
+++ Assignment1AA/src/utility/Container.java
@@ -20,33 +20,31 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Container {
-    //Attributes
+	// Attributes
 	@FXML
 	private Button clearButton, connectButton, send;
 	@FXML
 	private TextField displayName, serverIP;
-    @FXML
-    private MenuItem quitMenu;
-    @FXML
-    public TextArea chat, msg;
-    
-    private ClientGUI client;
-    
-    private Message message;
-    
-    private String userName;
-    
-    
-    //Methods
-    @FXML
-    private void printMessage(ActionEvent e)
-    {
-        Date timeStamp = new Date();
-        System.out.println(userName);
-        message = new Message(userName, msg.getText(), timeStamp);
-        
-        chat.appendText(message + "\n");
-    } 
+	@FXML
+	private MenuItem quitMenu;
+	@FXML
+	public TextArea chat, msg;
+
+	private ClientGUI client;
+
+	private Message message;
+
+	private String userName;
+
+	// Methods
+	@FXML
+	private void printMessage(ActionEvent e) {
+		Date timeStamp = new Date();
+		System.out.println(userName);
+		message = new Message(userName, msg.getText(), timeStamp);
+
+		chat.appendText(message + "\n");
+	}
 
 	@FXML
 	public void handleClearButtonAction(ActionEvent event) {
@@ -54,30 +52,14 @@ public class Container {
 		serverIP.setText("");
 	}
 
-	@FXML
-	public void handleConnectButtonAction(ActionEvent event) throws IOException {
-		client = new ClientGUI();
-		if (client.connectServer(displayName.getText(), serverIP.getText())) {
-		    System.out.println(userName);
-			Parent gameViewParent = FXMLLoader.load(getClass().getClassLoader().getResource("client/GameScreen.fxml"));
-			Scene gameScene = new Scene(gameViewParent);
-
-			// get existing window from loginScene
-			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			window.setScene(gameScene);
-			window.show();
-		} else {
-			System.out.println("error");
-		}
-		userName = displayName.getText();
-	}
+	
 
 	@FXML
 	public void handleQuitMenuAction(ActionEvent event) {
 		Platform.exit();
 		// http://www.learningaboutelectronics.com/Articles/How-to-add-functionality-to-a-menu-in-JavaFX.php
 	}
-	
+
 	@FXML
 	public void handleDisconnectButtonAction(ActionEvent event) {
 		Platform.exit();
